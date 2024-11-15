91c279b7d045ea8a7725cae09249a7871fc0cf50
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-11-15 05:50:06 -0500

    Complete Exercise 7: Add validations for Employee and Store models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,45 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# 1. Add validations to two models to enforce the following business rules:
+class Employee < ActiveRecord::Base
+  # - Employees must always have a first name present
+  validates :first_name, presence: true
+  # - Employees must always have a last name present
+  validates :last_name, presence: true
+  # - Employees have a hourly_rate that is a number (integer) between 40 and 200
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  # - Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+  validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+  # - Stores must always have a name that is a minimum of 3 characters
+  validates :name, presence: true, length: { minimum: 3 }
+  # - Stores have an annual_revenue that is a number (integer) that must be 0 or more
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # - BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Store must carry at least one of the men's or women's apparel")
+    end
+  end
+end
+
+# 2. Ask the user for a store name (store it in a variable)
+puts "Please enter a store name:"
+print "> "
+store_name = gets.chomp
+
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+new_store = Store.new(name: store_name)
+
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+if new_store.save
+  puts "Store with name #{store_name} created successfully!"
+else
+  puts "Failed to create store #{store_name}, Errors:"
+  new_store.errors.full_messages.each { |message| puts message }
+end
