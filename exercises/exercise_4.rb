d4527e2a4871b64da1d45804c66962352e16dfc0
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-11-15 03:49:23 -0500

    Complete Exercise 4: Load and display stores based on revenue conditions

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,19 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# 1. Borrowing and modifying the code from Exercise one, create 3 more stores:
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# 2. Using the where class method from Active Record, fetch (a collection of) only those stores that carry 
+# men's apparel. Assign the results to a variable @mens_stores.
+@mens_stores = Store.where(mens_apparel: true)
+
+# 3. Loop through each of these stores and output their name and annual revenue on each line.
+@mens_stores.each do |store|
+  puts "#{store.name} - #{store.annual_revenue}"
+end
+
+# 4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+@womens_stores_under_1M_revenue = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
